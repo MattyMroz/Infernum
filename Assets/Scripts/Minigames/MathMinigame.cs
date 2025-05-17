4d475c57daf0580d2fc8f2ea733f22a2495ca789
Wiktor Grzyb <251151@edu.p.lodz.pl>
2025-05-17 17:46:24 +0200

    Poprawa fokusu minigry

--- Assets/Scripts/Minigames/MathMinigame.cs
+++ Assets/Scripts/Minigames/MathMinigame.cs
@@ -1,68 +1,94 @@
+﻿using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;  
 
 public class MathMinigame : MonoBehaviour
 {
+  
+    [Header("UI")]
     [SerializeField] private GameObject _panel;
-    [SerializeField] TextMeshProUGUI displayNumber;
-    [SerializeField] TMP_InputField inputField;
+    [SerializeField] private TextMeshProUGUI displayNumber;
+    [SerializeField] private TMP_InputField inputField;
 
+    [SerializeField] private TextMeshProUGUI DisplayLvl;
+    [SerializeField] private TextMeshProUGUI DisplayExp;
+
+    [Header("Gameplay")]
     [SerializeField] private GameObject _player;
+    [SerializeField] private int mathGain = 10;
 
-
-
+  
     private int randomNumber;
+    private Player _playerScript;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private IEnumerator Start()
     {
+        _playerScript = _player.GetComponent<Player>();
+
+        inputField.contentType = TMP_InputField.ContentType.IntegerNumber;
+        inputField.characterValidation = TMP_InputField.CharacterValidation.Digit;
+        inputField.onValidateInput += OnlyDigits;
+
+        yield return null;
+
         GenerateNumber();
+
         inputField.onEndEdit.AddListener(CheckInput);
-        inputField.ActivateInputField();
+        inputField.ActivateInputField();          
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (!inputField.isFocused) return;
 
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            int len = inputField.text.Length;
+            inputField.caretPosition =
+            inputField.selectionAnchorPosition =
+            inputField.selectionFocusPosition = len;
+        }
     }
 
+    /* ---------- LOGIKA ► GENEROWANIE ---------- */
     void GenerateNumber()
     {
-        // Generate a random number between 100000000 and 999999999
-        randomNumber = Random.Range(100000000, 999999999);
-
-        // Display the number on the panel
+        randomNumber = Random.Range(1_000_000, 10_000_000);
         displayNumber.text = randomNumber.ToString();
+
+        UpdateHud();
+
+        inputField.text = string.Empty;
     }
+
 
     void CheckInput(string userInput)
     {
-        if (inputField != null)
+        if (string.IsNullOrWhiteSpace(userInput))
+            return;
+
+        if (int.TryParse(userInput, out int inputNumber) && inputNumber == randomNumber)
         {
-            inputField.text = userInput;
-        }
-        // Check if the input is correct
-        if (int.TryParse(userInput, out int inputNumber))
-        {
-            if (inputNumber == randomNumber)
-            {
-                // Correct answer
-                Debug.Log("Correct!");
-                //_panel.SetActive(false);
-                //_player.GetComponent<Player>().EndMinigame();
-            }
-            else
-            {
-                // Incorrect answer
-                Debug.Log("Incorrect! Try again.");
-                GenerateNumber();
-            }
-        }
-        else
-        {
-            Debug.Log("Invalid input! Please enter a number.");
+            _playerScript.exams_knowledge[0] += mathGain;
+            UpdateHud();
         }
 
+        GenerateNumber();
+
+        inputField.ActivateInputField();
+
     }
+
+    private char OnlyDigits(string text, int index, char addedChar) => char.IsDigit(addedChar) ? addedChar : '\0';
+
+    private void UpdateHud()
+    {
+        int lvl = _playerScript.exams_knowledge[0] / 100;
+        DisplayLvl.text = $"Level: {lvl}";
+        DisplayExp.text = $"Exp: {_playerScript.exams_knowledge[0]}/100";
+    }
+
+    public void FocusInput() => inputField.ActivateInputField();
 }
