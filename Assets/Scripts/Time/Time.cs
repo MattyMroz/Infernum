6bdd5039ad50cf3495f2350ad116a2a49f81c292
Wiktor Grzyb <251151@edu.p.lodz.pl>
2025-06-11 19:52:28 +0200

    Dodanie elementow UI i rework systemu Pausy

--- Assets/Scripts/Time/Time.cs
+++ Assets/Scripts/Time/Time.cs
@@ -1,67 +1,71 @@
-using UnityEngine;
+﻿using UnityEngine;
 using TMPro;
 
 public class Time : MonoBehaviour
 {
-     private static Time instance;
+    private static Time instance;
 
     public static float timer;
     public static bool timeStarted = true;
+    private static bool paused = false;
 
-    [SerializeField] TextMeshProUGUI statsText;
+    [SerializeField] private TextMeshProUGUI statsText;
 
-    private const float multiplier = (60 * 8) / 3; //8 hours in 3 minutes -> 60 minutes * 8 hours / 3 minutes
-    private readonly System.TimeSpan startTime = new System.TimeSpan(8, 0, 0); // 8:00
+    private const float multiplier = (60 * 8) / 3;
+    private readonly System.TimeSpan startTime = new System.TimeSpan(8, 0, 0);
     [SerializeField] private int days;
     [SerializeField] private string the_time;
-
-    void Update()
-    {
-        FlowOfTime();
-        UpdateUI();
-    }
-
-    private static void FlowOfTime()
-    {
-        if (timeStarted)
-        {
-            timer += UnityEngine.Time.deltaTime * multiplier; //* 100; add for testing
-        }
-    }
-
-    private void UpdateUI()
-    {
-        // Convert timer (in seconds) to a TimeSpan
-        System.TimeSpan currentTime = new System.TimeSpan(0, 0, (int)timer);
-        System.TimeSpan simulatedTime = startTime.Add(currentTime);
-
-        // new day
-        if (simulatedTime.TotalHours >= 16)
-        {
-            days++;
-            timer = 0;
-        }
-
-        // 2 weeks
-        if(days == 14)
-        {
-            simulatedTime = new System.TimeSpan(16, 0, 0);
-            timeStarted = false; // Stop time
-        }
-
-        // display time in hh:mm
-       statsText.text = "Day: " + days + "\n" +
-                         "Time: " + simulatedTime.ToString(@"hh\:mm");
-
-        the_time = simulatedTime.ToString(@"hh\:mm");
-    }
-
 
     void Awake()
     {
         instance = this;
     }
 
-public static int Days => instance.days;
-public static string Time_now => instance.the_time;
+    void Update()
+    {
+        if (timeStarted && !paused)
+        {
+            timer += UnityEngine.Time.deltaTime * multiplier;
+        }
+
+
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        System.TimeSpan currentTime = new System.TimeSpan(0, 0, (int)timer);
+        System.TimeSpan simulatedTime = startTime.Add(currentTime);
+
+        if (simulatedTime.TotalHours >= 16)
+        {
+            days++;
+            timer = 0;
+        }
+
+        if (days == 14)
+        {
+            simulatedTime = new System.TimeSpan(16, 0, 0);
+            timeStarted = false;
+        }
+
+        statsText.text = $"Day: {days}\nTime: {simulatedTime:hh\\:mm}";
+        the_time = simulatedTime.ToString(@"hh\:mm");
+    }
+
+    public static int Days => instance.days;
+    public static string Time_now => instance.the_time;
+
+    public static void PauseTime()
+    {
+        paused = true;
+    }
+
+    public static void ResumeTime()
+    {
+        if (timeStarted)
+        {
+            paused = false;
+        }
+    }
 }
